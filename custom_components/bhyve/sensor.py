c7edd12399bb89ddd5d3e02ab307a8901ec9e461
disforw <github@benzona.net>
2021-08-30 20:49:12 -0400

    Update sensor.py

--- custom_components/bhyve/sensor.py
+++ custom_components/bhyve/sensor.py
@@ -43,7 +43,6 @@ async def async_setup_platform(hass, config, async_add_entities, _discovery_info
             if device.get("battery", None) is not None:
                 sensors.append(BHyveBatterySensor(hass, bhyve, device))
         if device.get("type") == DEVICE_FLOOD:
-            sensors.append(BHyveFloodSensor(hass, bhyve, device))
             sensors.append(BHyveTempSensor(hass, bhyve, device))
             sensors.append(BHyveBatterySensor(hass, bhyve, device))
 
@@ -231,55 +230,8 @@ class BHyveStateSensor(BHyveDeviceEntity):
 
     def _should_handle_event(self, event_name, data):
         return event_name in [EVENT_CHANGE_MODE]
-    
-    
-class BHyveFloodSensor(BHyveDeviceEntity):
-    """Define a BHyve sensor."""
 
-    def __init__(self, hass, bhyve, device):
-        """Initialize the sensor."""
-        name = "{0} water sensor".format(device.get("name"))
-        _LOGGER.info("Creating state sensor: %s", name)
-        super().__init__(hass, bhyve, device, name, "information")
 
-    def _setup(self, device):
-        self._icon = "mdi:water"
-        self._device_class = "moisture"
-        self._state = "Wet" if device.get("status", {}).get("flood_alarm_status") == "alarm" else "Dry"
-        self._available = device.get("is_connected", False)
-        self._attrs = {
-            "location": device.get("location_name"),
-            "shutoff": device.get("auto_shutoff"),
-            "rssi": device.get("status", {}).get("rssi"),
-        }
-        _LOGGER.debug(
-            f"State sensor {self._name} setup: State: {self._state} | Available: {self._available}"
-        )
-
-    @property
-    def state(self):
-        """Return the state of the entity"""
-        return self._state
-
-    @property
-    def unique_id(self):
-        """Return a unique, unchanging string that represents this sensor."""
-        return f"{self._mac_address}:{self._device_id}:state"
-
-    def _on_ws_data(self, data):
-        """
-            {"last_flood_alarm_at":"2021-08-29T16:32:35.585Z","rssi":-60,"onboard_complete":true,"temp_f":75.2,"provisioned":true,"phy":"le_1m_1000","event":"fs_status_update","temp_alarm_status":"ok","status_updated_at":"2021-08-29T16:33:17.089Z","identify_enabled":false,"device_id":"612ad9134f0c6c9c9faddbba","timestamp":"2021-08-29T16:33:17.089Z","flood_alarm_status":"ok","last_temp_alarm_at":null}
-        """
-        _LOGGER.info("Received program data update {}".format(data))
-        event = data.get("event")
-        if event == EVENT_FS_ALARM:
-            self._state = "Wet" if data.get("flood_alarm_status") == "alarm" else "Dry"
-            self._attrs['rssi'] = data.get("rssi")
-
-    def _should_handle_event(self, event_name, data):
-        return event_name in [EVENT_FS_ALARM]
-
-    
 class BHyveTempSensor(BHyveDeviceEntity):
     """Define a BHyve sensor."""
 
