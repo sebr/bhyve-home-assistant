cde3dac0d4fdc4ceb33cb3f9b9e8f807a6d2a195
disforw <github@benzona.net>
2023-04-30 22:38:05 -0400

    Update binary_sensor.py

--- custom_components/bhyve/binary_sensor.py
+++ custom_components/bhyve/binary_sensor.py
@@ -28,6 +28,7 @@ async def async_setup_entry(
     for device in devices:
         if device.get("type") == DEVICE_FLOOD:
             sensors.append(BHyveFloodSensor(hass, bhyve, device))
+            sensors.append(BHyveTemperatureAlert(hass, bhyve, device))
 
     async_add_entities(sensors, True)
 
@@ -84,6 +85,43 @@ class BHyveFloodSensor(BHyveDeviceEntity):
         if event == EVENT_FS_ALARM:
             self._state = self._parse_status(data)
             self._attrs["rssi"] = data.get("rssi")
+
+    def _should_handle_event(self, event_name, data):
+        return event_name in [EVENT_FS_ALARM]
+
+    
+class BHyveTemperatureAlert(BHyveDeviceEntity):
+
+    def __init__(self, hass, bhyve, device):
+        name = "{} temperature alert".format(device.get("name"))
+        super().__init__(hass, bhyve, device, name, "alert")
+
+    def _setup(self, device):
+        self._available = device.get("is_connected", False)
+        self._state = device.get("status", {}).get("temp_alarm_status")
+
+    @property
+    def state(self):
+        """Return the state of the entity."""
+        return self._state
+
+    @property
+    def unique_id(self):
+        """Return the unique id."""
+        return f"{self._mac_address}:{self._device_id}:tempalert"
+
+    @property
+    def is_on(self):
+        return self._state == "on"
+
+    def _on_ws_data(self, data):
+        """
+        {"last_flood_alarm_at":"2021-08-29T16:32:35.585Z","rssi":-60,"onboard_complete":true,"temp_f":75.2,"provisioned":true,"phy":"le_1m_1000","event":"fs_status_update","temp_alarm_status":"ok","status_updated_at":"2021-08-29T16:33:17.089Z","identify_enabled":false,"device_id":"612ad9134f0c6c9c9faddbba","timestamp":"2021-08-29T16:33:17.089Z","flood_alarm_status":"ok","last_temp_alarm_at":null}
+        """
+        _LOGGER.info("Received program data update %s", data)
+        event = data.get("event")
+        if event == EVENT_FS_ALARM:
+            self._state = self._parse_status(data)
 
     def _should_handle_event(self, event_name, data):
         return event_name in [EVENT_FS_ALARM]
