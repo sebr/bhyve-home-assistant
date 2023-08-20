530039f0bcb48ad88218db970cb27dc494b4dfa9
Seb Ruiz <seb.ruiz@calven.com>
2023-08-20 14:09:45 +1000

    Improve debug scripts

--- debug/debug.py
+++ debug/debug.py
@@ -9,11 +9,11 @@ if len(sys.argv) < 2:
     print("Not enough arguments")
     sys.exit()
 
-folder = sys.argv[1]
+FOLDER = sys.argv[1]
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
-diagnostics_json = os.path.join(script_dir, folder + "/diagnostics.json")
+diagnostics_json = os.path.join(script_dir, FOLDER + "/diagnostics.json")
 
 with open(diagnostics_json, mode="r", encoding="utf8") as diagnosticsFile:
     diagnostics = json.load(diagnosticsFile)
@@ -25,18 +25,16 @@ for device in diagnostics.get("data").get("devices"):
 
     name = device.get("name", "Unknown Name")
     print("")
+    print(f"===============  {name}  ====================")
+    print(f"{'Type':>10}: {device.get('type')}")
+    print(f"{'id':>10}: {device.get('id')}")
+    print(f"{'Last seen':>10}: {device.get('last_connected_at')}")
+    print(f"{'Battery':>10}: {device.get('battery')}")
 
     if device.get("type") == "sprinkler_timer":
         zones = device.get("zones")
-        print(f"{'Sprinkler':>10}: {name}")
-        print(f"{'id':>10}: {device.get('id')}")
-        print(f"{'Battery':>10}: {device.get('battery')}")
         print(f"{'Zones':>10}: {len(zones)}")
         for zone in device.get("zones"):
             zn = zone.get("name", "Unknown Zone")
             zid = zone.get("station")
-            print(f"            [{zid:<2}] {zn}")
-
-    if device.get("type") == "flood_sensor":
-        print("Flood sensor:")
-        print(f"  Name: {name:<6}")
+            print(f"            [{zid:>2}] {zn}")
